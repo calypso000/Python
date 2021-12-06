31af8cc561f03eda4377584495fb9114496dc642
Calypso  Farel <calypsombengmo1@gmail.com>
2021-12-06 13:54:57 +0100

    amelioration du formulaire

---
+++ compte/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+
+    path('inscription',views.inscriptionPage,name='inscription'),
+    path('connexion', views.connexionPage, name='connexion'),
+
+]
